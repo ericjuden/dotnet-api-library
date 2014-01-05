573929b0e99fabd381371b6272a935a59e63df25
cjhowson <cjhowson@gmail.com>
2014-01-05 12:47:14 +0000

    Updated assembly version to 2.0.0

--- src/KayakoRestAPI/Properties/AssemblyInfo.cs
+++ src/KayakoRestAPI/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using System;
 //     - MajorVersion - Substantial changes and overhauls
 //     - Feature - When a new feature is added
 //     - MinorVersion - When changing an existing feature
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: InternalsVisibleTo("KayakoRestApi.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001571a35d72e2fcf1c9256b272d5b09c1272220e11dce2eb8bd44d29d1ff69247f0c0a3a5655c4729a31e36e8f341628740e5e5877f306eb3f27ef806d22a2b1af01b7fee15cfd6cc8dee15ffe5ad8ee59fd5e39f9893171e6613e6a4b7510a1d8845add0caa0d9e0abe3f63ddc5f6496e84d0ad191d8eaa69b07b1148126ecce")]
 [assembly: InternalsVisibleTo("KayakoRestApi.IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001571a35d72e2fcf1c9256b272d5b09c1272220e11dce2eb8bd44d29d1ff69247f0c0a3a5655c4729a31e36e8f341628740e5e5877f306eb3f27ef806d22a2b1af01b7fee15cfd6cc8dee15ffe5ad8ee59fd5e39f9893171e6613e6a4b7510a1d8845add0caa0d9e0abe3f63ddc5f6496e84d0ad191d8eaa69b07b1148126ecce")]
 
